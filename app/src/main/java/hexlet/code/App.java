11628a8cab089f5a578a16b3013bce9d636906dd
stat200 <v.bilyuga@yandex.ru>
2023-05-14 19:22:51 +0300

    step3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import hexlet.code.Cli;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Cli.greetings();
     }
 }
